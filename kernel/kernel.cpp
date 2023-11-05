76bb5a60b4528e151cb3044e0b2ed951479538c2
linewelder <redalerm@gmail.com>
2023-11-06 00:25:38 +0100

    Add echoing of what is being typed

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,14 +1,17 @@
+#include <stdint.h>
+
+#include "ps2.h"
+#include "keyboard.h"
 #include "terminal.h"
 
 extern "C" void kmain() {
     terminal::clear();
-    terminal::write_cstr("Hello, world!\n");
+    terminal::write_cstr("Los\n");
 
-    terminal::set_color(terminal::Color::WHITE, terminal::Color::RED);
-    for (int i = 0; i < 256; i++) {
-        terminal::putchar(static_cast<char>(i));
-        if (i % 16 == 15) {
-            terminal::putchar('\n');
-        }
+    ps2::disable_translation();
+    ps2::enable_scanning();
+
+    for (;;) {
+        terminal::putchar(keyboard::poll_char());
     }
 }
