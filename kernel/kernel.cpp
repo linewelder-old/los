822dd0edc1639bdd70d0f815a2d885c6a9e83405
linewelder <redalerm@gmail.com>
2023-11-07 00:00:33 +0100

    Create rudimentary keyboard detection

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -3,13 +3,21 @@
 #include "ps2.h"
 #include "keyboard.h"
 #include "terminal.h"
+#include "printf.h"
 
 extern "C" void kmain() {
     terminal::clear();
     terminal::write_cstr("Los\n");
 
     ps2::init();
-    ps2::first.enable_scanning();
+    if (ps2::first.get_type() == 0xab83) {
+        ps2::first.enable_scanning();
+    } else if (ps2::second.get_type() == 0xab83) {
+        ps2::second.enable_scanning();
+    } else {
+        printf("No keyboard.\n");
+        for (;;);
+    }
 
     for (;;) {
         terminal::putchar(keyboard::poll_char());
