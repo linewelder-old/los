1336dce2a93ad926bb6a35558ba49466499ea962
linewelder <redalerm@gmail.com>
2023-11-06 01:24:28 +0100

    Add printf

---
+++ kernel/printf.cpp
@@ -0,0 +1,113 @@
+#include <limits.h>
+#include <stdarg.h>
+
+#include "terminal.h"
+
+static int print_number(int value, int base, int max_length) {
+    int written = 0;
+
+    if (value == 0) {
+        if (written == max_length) return -1;
+        written++;
+        terminal::putchar('0');
+    }
+
+    constexpr int BUF_SIZE = 12;
+    char buf[BUF_SIZE];
+    int buf_start = BUF_SIZE;
+
+    while (value) {
+        buf_start--;
+        int digit = value % base;
+        if (digit < 10) {
+            buf[buf_start] = '0' + digit;
+        } else {
+            buf[buf_start] = 'a' + digit - 10;
+        }
+
+        value /= base;
+    }
+
+    for (int k = buf_start; k < BUF_SIZE; k++) {
+        if (written == max_length) return -1;
+        written++;
+        terminal::putchar(buf[k]);
+    }
+
+    return written;
+}
+
+/** Supports only %d %x %c %s formatters without precision and width. */
+int printf(const char* format, ...) {
+    va_list params;
+    va_start(params, format);
+
+    int written = 0;
+    while (*format != '\0') {
+        size_t remained = INT_MAX - written;
+
+        if (format[0] == '%') {
+            switch (format[1]) {
+                case 'd': {
+                    int i = va_arg(params, int);
+                    int result = print_number(i, 10, remained);
+                    if (result < 0) {
+                        return -1;
+                    }
+                    break;
+                }
+
+                case 'x': {
+                    int i = va_arg(params, int);
+                    int result = print_number(i, 16, remained);
+                    if (result < 0) {
+                        return -1;
+                    }
+                    break;
+                }
+
+                case 'c': {
+                    char ch = (char)va_arg(params, int); // char promotes to int.
+                    if (remained == 0) return -1;
+                    terminal::putchar(ch);
+                    written++;
+                    break;
+                }
+
+                case 's': {
+                    const char* str = va_arg(params, const char*);
+                    while (str[0]) {
+                        if (remained == 0) return -1;
+                        terminal::putchar(str[0]);
+                        written++;
+                        str++;
+                    }
+                    break;
+                }
+
+                case '%': {
+                    if (remained == 0) return -1;
+                    written++;
+                    terminal::putchar('%');
+                    break;
+                }
+
+                default: {
+                    if (remained == 0) return -1;
+                    written++;
+                    terminal::putchar('%');
+                    format++;
+                    continue;
+                }
+            }
+
+            format += 2;
+        } else {
+            terminal::putchar(format[0]);
+            format++;
+        }
+    }
+
+    va_end(params);
+    return written;
+}
---
+++ kernel/printf.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/** Supports only %d %x %c %s formatters without precision and width. */
+int printf(const char* format, ...);
