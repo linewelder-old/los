837962c0be96417b01310e0ad42f25f38ada1c37
linewelder <redalerm@gmail.com>
2023-12-02 02:43:30 +0100

    Wrap logging in namespace

--- kernel/include/kernel/log.h
+++ kernel/include/kernel/log.h
@@ -1,13 +1,15 @@
 #pragma once
 
-enum class SeverityLevel {
-    INFO,
-    WARNING,
-    ERROR,
-};
+namespace log {
+    enum class SeverityLevel {
+        INFO,
+        WARNING,
+        ERROR,
+    };
 
-void log(SeverityLevel severity, const char* file, int line, const char* format, ...);
+    void log(SeverityLevel severity, const char* file, int line, const char* format, ...);
+}
 
-#define LOG_INFO(...) log(SeverityLevel::INFO, __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_WARN(...) log(SeverityLevel::WARNING, __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...) log(SeverityLevel::ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...) log::log(log::SeverityLevel::INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...) log::log(log::SeverityLevel::WARNING, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) log::log(log::SeverityLevel::ERROR, __FILE__, __LINE__, __VA_ARGS__)
--- kernel/kernel/log.cpp
+++ kernel/kernel/log.cpp
@@ -5,24 +5,24 @@
 #include <arch/i386/terminal.h>
 #include <kernel/printf.h>
 
-static const char* get_severity_str(SeverityLevel level) {
-    switch (level) {
-        case SeverityLevel::INFO:    return "INF";
-        case SeverityLevel::WARNING: return "WRN";
-        case SeverityLevel::ERROR:   return "ERR";
-        default: __builtin_unreachable();
+namespace log {
+    static const char* get_severity_str(SeverityLevel level) {
+        switch (level) {
+            case SeverityLevel::INFO:    return "INF";
+            case SeverityLevel::WARNING: return "WRN";
+            case SeverityLevel::ERROR:   return "ERR";
+            default: __builtin_unreachable();
+        }
     }
-}
 
-void log(SeverityLevel severity, const char* file, int line, const char* format, ...) {
-    va_list args;
-    va_start(args, format);
+    void log(SeverityLevel severity, const char* file, int line, const char* format, ...) {
+        va_list args;
+        va_start(args, format);
 
-    terminal::set_color(terminal::Color::DARK_GREY, terminal::Color::BLACK);
-    printf("%s [%s:%d] ", get_severity_str(severity), file, line);
-    vprintf(format, args);
-    terminal::putchar('\n');
-    terminal::set_color(terminal::Color::LIGHT_GREY, terminal::Color::BLACK);
+        printf("%s [%s:%d] ", get_severity_str(severity), file, line);
+        vprintf(format, args);
+        terminal::putchar('\n');
 
-    va_end(args);
+        va_end(args);
+    }
 }
