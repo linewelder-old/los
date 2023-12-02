0f22f1b1c05a1eb2c2e48416284778dfecf5162d
linewelder <redalerm@gmail.com>
2023-12-02 02:41:48 +0100

    Create enum for log severity level

--- kernel/include/kernel/log.h
+++ kernel/include/kernel/log.h
@@ -1,7 +1,13 @@
 #pragma once
 
-void log(const char* severity, const char* file, int line, const char* format, ...);
+enum class SeverityLevel {
+    INFO,
+    WARNING,
+    ERROR,
+};
 
-#define LOG_INFO(...) log("INF", __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_WARN(...) log("WRN", __FILE__, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...) log("ERR", __FILE__, __LINE__, __VA_ARGS__)
+void log(SeverityLevel severity, const char* file, int line, const char* format, ...);
+
+#define LOG_INFO(...) log(SeverityLevel::INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...) log(SeverityLevel::WARNING, __FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) log(SeverityLevel::ERROR, __FILE__, __LINE__, __VA_ARGS__)
--- kernel/kernel/log.cpp
+++ kernel/kernel/log.cpp
@@ -5,13 +5,24 @@
 #include <arch/i386/terminal.h>
 #include <kernel/printf.h>
 
-void log(const char* severity, const char* file, int line, const char* format, ...) {
+static const char* get_severity_str(SeverityLevel level) {
+    switch (level) {
+        case SeverityLevel::INFO:    return "INF";
+        case SeverityLevel::WARNING: return "WRN";
+        case SeverityLevel::ERROR:   return "ERR";
+        default: __builtin_unreachable();
+    }
+}
+
+void log(SeverityLevel severity, const char* file, int line, const char* format, ...) {
     va_list args;
     va_start(args, format);
 
-    printf("%s [%s:%d] ", severity, file, line);
+    terminal::set_color(terminal::Color::DARK_GREY, terminal::Color::BLACK);
+    printf("%s [%s:%d] ", get_severity_str(severity), file, line);
     vprintf(format, args);
     terminal::putchar('\n');
+    terminal::set_color(terminal::Color::LIGHT_GREY, terminal::Color::BLACK);
 
     va_end(args);
 }
