49243ee1390d9688f35099e73c18921456174a5a
linewelder <redalerm@gmail.com>
2023-12-02 02:50:19 +0100

    Color log messages based on severity level

--- kernel/kernel/log.cpp
+++ kernel/kernel/log.cpp
@@ -6,11 +6,16 @@
 #include <kernel/printf.h>
 
 namespace log {
-    static const char* get_severity_str(SeverityLevel level) {
+    struct SeverityStyle {
+        const char* name;
+        terminal::Color color;
+    };
+
+    static SeverityStyle get_severity_style(SeverityLevel level) {
         switch (level) {
-            case SeverityLevel::INFO:    return "INF";
-            case SeverityLevel::WARNING: return "WRN";
-            case SeverityLevel::ERROR:   return "ERR";
+            case SeverityLevel::INFO:    return { "INF", terminal::Color::DARK_GREY };
+            case SeverityLevel::WARNING: return { "WRN", terminal::Color::LIGHT_BROWN };
+            case SeverityLevel::ERROR:   return { "ERR", terminal::Color::LIGHT_RED };
             default: __builtin_unreachable();
         }
     }
@@ -19,9 +24,13 @@ namespace log {
         va_list args;
         va_start(args, format);
 
-        printf("%s [%s:%d] ", get_severity_str(severity), file, line);
+        SeverityStyle style = get_severity_style(severity);
+        terminal::set_color(style.color, terminal::Color::BLACK);
+
+        printf("%s [%s:%d] ", style.name, file, line);
         vprintf(format, args);
         terminal::putchar('\n');
+        terminal::set_color(terminal::Color::LIGHT_GREY, terminal::Color::BLACK);
 
         va_end(args);
     }
